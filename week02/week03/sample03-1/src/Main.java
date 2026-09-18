8ca36228c068ea0240541827de7ebdfb2997031c
Mehrona <ashurovamehrona599@gmail.com>
2026-09-18 07:23:03 +0500

    3추자 실십

---
+++ week02/week03/sample03-1/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    byte value1=127; // -128 ~ 127
+    int value2 = 40000;
+    short value22 = 32767;
+    long value3 = 40000l;
+    double value4 = 3.14;
+    float value5 = 3.14f;
+    char value6 = '가';
+    String value7 = "가";
+
+    System.out.printf("%d + 1 = %d\n", value1, value1 + 1);
+}
---
+++ week02/week03/sample03-4/src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+        Scanner keyboard = new Scanner(System.in);
+        int base;
+        int rectagular_area;
+        double radius;
+        double circle_area;
+        final double PI = 3.141592;
+        double area;
+
+
+        System.out.print("정사각형의 한변의 길이 입력(예 5) : ");
+        base = keyboard.nextInt();
+
+        rectagular_area = base * base;
+        radius = base / 2.0;
+        circle_area = PI * radius* radius;
+        area = rectagular_area - circle_area;
+
+
+        System.out.printf("한변의 길이나 %,d Cm인 정사각형의 면적 + %,d \u33A0\n", base, rectagular_area);
+        System.out.printf("이 정사각형 내부 원의 반지름 : %.2f Cm, 면적 : %,.2f \u33A0\n", radius, circle_area);
+        System.out.printf("구하려는 면적 : %,.2f\u33a0\n", area);
+}
